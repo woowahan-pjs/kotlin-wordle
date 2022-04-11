9e6149d7e983a0444df23dafa673a94fcfe11ebf
ohgillwhan <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>
2022-04-12 08:31:23 +0900

    feat(Tile.kt): 알파벳만 입력이 가능하다.

--- src/main/kotlin/domain/Tile.kt
+++ src/main/kotlin/domain/Tile.kt
@@ -1,6 +1,8 @@
 package domain
 
 @JvmInline
-value class Tile (
-    val character: Char
-)
+value class Tile(val character: Char) {
+    init {
+        require(character in 'A'..'z') { "문자는 알파벳만 입력이 가능합니다." }
+    }
+}
--- src/test/kotlin/domain/TileTest.kt
+++ src/test/kotlin/domain/TileTest.kt
@@ -1,15 +1,26 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class TileTest {
     @Test
     fun `타일은 하나의 문자를 가지고 있다`() {
         // given
-        val tile = Tile('쿄')
+        val tile = Tile('a')
 
         // then
-        assertThat(tile.character).isEqualTo('쿄')
+        assertThat(tile.character).isEqualTo('a')
+    }
+
+    @ParameterizedTest
+    @ValueSource(chars = [' ', '0', '1', '5', '9', 'ㄱ', '⬜'])
+    fun `타일은 알파벳만 입력이 가능하다`(character: Char) {
+        assertThatIllegalArgumentException()
+            .isThrownBy { Tile(character) }
+            .withMessage("문자는 알파벳만 입력이 가능합니다.")
     }
 }
