ac876f82d33a68b8ffba695bf6dbe66f375895d3
ohgillwhan <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>
2022-04-12 22:30:32 +0900

    feat(Answer.kt) : Answer는 5개의 Tile로 구성이 되어있다.

---
+++ src/test/kotlin/domain/AnswerTest.kt
@@ -0,0 +1,32 @@
+package domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class AnswerTest {
+    @Test
+    fun `Answer는 5개의 Tile로 구성이 되어있다`() {
+        // given
+        val elements = listOf(Tile('a'), Tile('b'), Tile('c'), Tile('d'), Tile('e'))
+
+        // when
+        val answer = Answer(elements)
+
+        // then
+        Assertions.assertThat(answer.tiles).containsExactlyElementsOf(elements)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3, 4, 6, 10])
+    fun `Answer는 5개의 Tile로 구성이 안되면 실패한다`(size: Int) {
+        // given
+        val elements: List<Tile> = (0 until size).map { Tile('a') }
+
+        // then
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Answer(elements) }
+            .withMessage("타일은 5개로 구성되어야 합니다.")
+    }
+}
