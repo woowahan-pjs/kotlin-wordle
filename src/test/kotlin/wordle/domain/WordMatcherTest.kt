cbf8f749cb4a49eeb56e411d9ea41ba9dbbc29b5
sgkim94 <tkdrn8578@naver.com>
2022-04-14 10:52:43 +0900

    feat: junit test import 추가

--- src/test/kotlin/wordle/domain/WordMatcherTest.kt
+++ src/test/kotlin/wordle/domain/WordMatcherTest.kt
@@ -1,6 +1,7 @@
 package wordle.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
