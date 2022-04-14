1d39fa965d2218964e643d5cb14b39ab1cf54722
sgkim94 <tkdrn8578@naver.com>
2022-04-14 10:52:01 +0900

    refactor: private 접근제어자 추가

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -27,6 +27,6 @@ data class Word(val value: String) {
         private const val WORD_SIZE = 5
         private const val WRONG_WORD_SIZE_MESSAGE = "5글자여야합니다"
         private const val INDEX_OUT_RANGE_MESSAGE = "인덱스 범위를 초과했습니다."
-        val ALPHABET_REGEX = Regex("^[a-zA-Z]*$")
+        private val ALPHABET_REGEX = Regex("^[a-zA-Z]*$")
     }
 }
