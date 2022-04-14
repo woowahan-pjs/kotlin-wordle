b06d616f5e23356f733acc8af1d5fd4055e69a15
sgkim94 <tkdrn8578@naver.com>
2022-04-14 17:53:04 +0900

    feat: Tiles value object 추가 및 테스트코드 추가

---
+++ src/main/kotlin/wordle/domain/Tiles.kt
@@ -0,0 +1,18 @@
+package wordle.domain
+
+class Tiles(private val tiles: List<Tile>) {
+
+    fun isWinner(): Boolean {
+        val count = tiles.count { it == Tile.GREEN }
+
+        if (count == TOTAL_TILE_COUNT) {
+            return true
+        }
+
+        return false
+    }
+
+    companion object {
+        private const val TOTAL_TILE_COUNT = 5
+    }
+}
