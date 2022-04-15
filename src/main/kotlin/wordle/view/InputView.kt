5bce301d0204e4e8367e179b11cc351f4909bcde
sgkim94 <tkdrn8578@naver.com>
2022-04-15 15:40:20 +0900

    refactor: readlin() 으로 변경

--- src/main/kotlin/wordle/view/InputView.kt
+++ src/main/kotlin/wordle/view/InputView.kt
@@ -6,7 +6,7 @@ object InputView {
 
     fun askWord(words: List<Word>): String {
         while (true) {
-            val input = question("정답을 입력해 주세요.")
+            val input = question()
             if (words.contains(Word(input))) {
                 return input
             }
@@ -15,8 +15,8 @@ object InputView {
         }
     }
 
-    private tailrec fun question(question: String): String {
-        println(question)
-        return readLine() ?: question(question)
+    private fun question(): String {
+        println("정답을 입력해 주세요.")
+        return readln()
     }
 }
