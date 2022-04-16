c40742d779d4435b8d047ea3993e97051fcf0675
etff <wish2rich@gmail.com>
2022-04-16 13:41:11 +0900

    refactor: 주석 삭제 - 주석내역 반영

--- src/main/kotlin/wordle/domain/Answers.kt
+++ src/main/kotlin/wordle/domain/Answers.kt
@@ -9,7 +9,6 @@ class Answers {
     private fun createAnswer() = WORDS.findAnswer()
 
     companion object {
-        // 선언 위치 확인 필요
         private const val ANSWERS_TEXT_PATH = "./words.txt"
         val WORDS = createWords()
 
@@ -18,7 +17,6 @@ class Answers {
             return Words(wordsFile.map { Word(it) })
         }
 
-        // 파일 읽는 부분만 목킹이 되면 될 듯
         private fun getResourceText(): File {
             return File(ClassLoader.getSystemResource(ANSWERS_TEXT_PATH).file)
         }
--- src/main/kotlin/wordle/domain/Words.kt
+++ src/main/kotlin/wordle/domain/Words.kt
@@ -3,7 +3,6 @@ package wordle.domain
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-// 전략패턴 필요
 class Words(private val words: List<Word>) {
 
     fun findAnswer(): Word {
