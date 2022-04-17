1f1a3af37527a764f4438f0bb5ef89346a8cd9fd
sgkim94 <tkdrn8578@naver.com>
2022-04-17 12:08:06 +0900

    refactor: 불필요한 import 제거

--- src/main/kotlin/wordle/domain/Words.kt
+++ src/main/kotlin/wordle/domain/Words.kt
@@ -1,9 +1,5 @@
 package wordle.domain
 
-import java.time.LocalDate
-import java.time.temporal.ChronoUnit
-
-// 전략패턴 필요
 class Words(private val words: List<Word>) {
 
     fun findAnswer(position: Int): Word {
