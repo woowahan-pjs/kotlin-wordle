8bdfa9d7757d006c44d78b76d63e41fa328ec70b
sgkim94 <tkdrn8578@naver.com>
2022-04-17 12:06:35 +0900

    refactor: require 사용하도록 변경

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -12,10 +12,7 @@ value class Word(val value: String) {
     private fun isWordSizeAndAlphabet() = value.length == WORD_SIZE && ALPHABET_REGEX.matches(value)
 
     fun findAlphabet(index: Int): String {
-        // require 변경해야 됨
-        if (index < FIRST_INDEX || index > value.length) {
-            throw IllegalArgumentException(INDEX_OUT_RANGE_MESSAGE)
-        }
+        require (index >= FIRST_INDEX && index <= value.length) { INDEX_OUT_RANGE_MESSAGE }
 
         return value[index].toString()
     }
