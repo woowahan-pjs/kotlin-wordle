d9a554dd7c7bf5c08b31d0fc78350a6e859c88c2
jojogreen91 <iawbg13@gmail.com>
2022-05-12 16:27:47 +0900

    feat: 어플리케이션을 실행하는 기능 구현

---
+++ src/main/kotlin/wordle/controller/WordleController.kt
@@ -0,0 +1,23 @@
+package wordle.controller
+
+import wordle.domain.Answer
+import wordle.domain.Game
+import wordle.domain.Results
+import wordle.domain.Words
+import wordle.view.printInputMessage
+import wordle.view.printResults
+import wordle.view.printStartMessage
+import java.time.LocalDate
+
+class WordleController {
+
+    fun run() {
+        printStartMessage()
+        val game = Game(Words.pick(LocalDate.now()), Results())
+        while (game.isPlaying) {
+            val answer = Answer(printInputMessage())
+            game.playRound(answer)
+            printResults(game.results, game.isPlaying, game.findTryCount())
+        }
+    }
+}
