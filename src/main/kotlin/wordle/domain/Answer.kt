58cbf753f039e27128fa1362cb4399fca7c0bde3
jojogreen91 <iawbg13@gmail.com>
2022-05-12 15:13:17 +0900

    feat: 답안과 정답을 비교하는 기능 구현

--- src/main/kotlin/wordle/domain/Answer.kt
+++ src/main/kotlin/wordle/domain/Answer.kt
@@ -1,9 +1,78 @@
 package wordle.domain
 
+private const val WORD_SIZE = 5
+
 class Answer(private val answer: String) {
 
     init {
-        require(answer.length == 5) { "[ERROR] 부적절한 글자 길이입니다." }
+        require(answer.length == WORD_SIZE) { "[ERROR] 부적절한 글자 길이입니다." }
         require(Words.contains(answer)) { "[ERROR] 목록에 존재하지 않는 단어입니다." }
     }
+
+    fun match(word: String): MutableList<Mark> {
+        val wordTable = createWordTable(word)
+        val result = MutableList(WORD_SIZE) { Mark.NONE }
+        matchExact(word, result, wordTable)
+        matchExist(result, wordTable)
+        return result
+    }
+
+    private fun createWordTable(word: String): HashMap<Char, Int> {
+        val wordTable = HashMap<Char, Int>()
+        for (it in word) {
+            wordTable[it] = wordTable.getOrDefault(it, 0) + 1
+        }
+        return wordTable
+    }
+
+    private fun matchExact(
+        word: String,
+        result: MutableList<Mark>,
+        wordTable: HashMap<Char, Int>,
+    ) {
+        for (i in 0 until WORD_SIZE) {
+            markExact(word, i, result, wordTable)
+        }
+    }
+
+    private fun markExact(
+        word: String,
+        i: Int,
+        result: MutableList<Mark>,
+        wordTable: HashMap<Char, Int>,
+    ) {
+        val key = word[i]
+        if (key == answer[i]) {
+            result[i] = Mark.EXACT
+            wordTable.computeIfPresent(key) { _, v -> v - 1 }
+        }
+    }
+
+    private fun matchExist(
+        result: MutableList<Mark>,
+        wordTable: HashMap<Char, Int>,
+    ) {
+        for (i in 0 until WORD_SIZE) {
+            markExist(i, result, wordTable)
+        }
+    }
+
+    private fun markExist(
+        i: Int,
+        result: MutableList<Mark>,
+        wordTable: HashMap<Char, Int>,
+    ) {
+        val key = answer[i]
+        if (isExist(result, i, wordTable, key)) {
+            result[i] = Mark.EXIST
+            wordTable.computeIfPresent(key) { _, v -> v - 1 }
+        }
+    }
+
+    private fun isExist(
+        result: MutableList<Mark>,
+        i: Int,
+        wordTable: HashMap<Char, Int>,
+        key: Char,
+    ) = result[i] == Mark.NONE && wordTable.containsKey(key) && wordTable[key] != 0
 }
--- src/test/kotlin/wordle/domain/AnswerTest.kt
+++ src/test/kotlin/wordle/domain/AnswerTest.kt
@@ -1,8 +1,10 @@
 package wordle.domain
 
 import io.kotest.matchers.throwable.shouldHaveMessage
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import wordle.domain.Mark.*
 
 internal class AnswerTest {
 
@@ -16,5 +18,13 @@ internal class AnswerTest {
     fun 주어진_단어목록에_존재하지_않는_경우_예외발생() {
         assertThrows<IllegalArgumentException> { Answer("abcde") }
             .shouldHaveMessage("[ERROR] 목록에 존재하지 않는 단어입니다.")
+    }
+
+    @Test
+    fun 답안과_정답을_비교() {
+        val answer = Answer("death")
+
+        Assertions.assertThat(answer.match("fresh"))
+            .isEqualTo(listOf(NONE, EXIST, NONE, NONE, EXACT))
     }
 }
