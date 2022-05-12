c1d7d33055c714b7cb66247a027b49bc82aa4de1
jojogreen91 <iawbg13@gmail.com>
2022-05-12 11:30:13 +0900

    test: AnswerTest 테스트작성

--- src/main/kotlin/wordle/domain/Answer.kt
+++ src/main/kotlin/wordle/domain/Answer.kt
@@ -1,8 +1,9 @@
 package wordle.domain
 
-class Answer(val answer: String) {
+class Answer(private val answer: String) {
 
     init {
-        require(answer.length == 5) { "[ERROR] 부적절한 글자 길이입니다."}
+        require(answer.length == 5) { "[ERROR] 부적절한 글자 길이입니다." }
+//        require(Words.contains(answer)) {}
     }
 }
--- src/test/kotlin/wordle/domain/AnswerTest.kt
+++ src/test/kotlin/wordle/domain/AnswerTest.kt
@@ -11,4 +11,10 @@ internal class AnswerTest {
         assertThrows<IllegalArgumentException> { Answer("abcdef") }
             .shouldHaveMessage("[ERROR] 부적절한 글자 길이입니다.")
     }
+
+    @Test
+    fun 주어진_단어목록에_존재하지_않는_경우_예외발생() {
+        assertThrows<IllegalArgumentException> { Answer("abcde") }
+            .shouldHaveMessage("[ERROR] 목록에 존재하지 않는 단어입니다.")
+    }
 }
