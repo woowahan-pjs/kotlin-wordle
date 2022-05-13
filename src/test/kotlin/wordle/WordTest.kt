490274e6e25ba6c92340d7ed267ad430a05738a3
BeomsooKim <kbsat@naver.com>
2022-05-13 17:25:39 +0900

    test: 테스트에 forAll 사용

--- src/test/kotlin/wordle/WordTest.kt
+++ src/test/kotlin/wordle/WordTest.kt
@@ -1,10 +1,9 @@
 package wordle
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class WordTest {
 
@@ -16,12 +15,13 @@ class WordTest {
         exception.message shouldBe "단어의 길이는 5글자여야 합니다."
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = ["ap pl", "ap피le"])
-    fun `영어가 아닌 글자가 포함된 word를 생성하면 예외가 발생한다`(word: String) {
-        val exception = shouldThrow<IllegalArgumentException> {
-            Word(word)
+    @Test
+    fun `영어가 아닌 글자가 포함된 word를 생성하면 예외가 발생한다`() {
+        listOf("ap pl", "ap피le").forAll {
+            val exception = shouldThrow<IllegalArgumentException> {
+                Word(it)
+            }
+            exception.message shouldBe "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다."
         }
-        exception.message shouldBe "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다."
     }
 }
