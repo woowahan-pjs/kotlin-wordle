9adf4b79d7d3dbdfc7bd4aa2419d4224a79b4786
BeomsooKim <kbsat@naver.com>
2022-05-13 17:22:51 +0900

    refactor: 클래스 이름 수정

--- src/main/kotlin/wordle/Word.kt
+++ src/main/kotlin/wordle/Word.kt
@@ -1,9 +1,12 @@
 package wordle
 
-class GameWord(private val word: String) {
+class Word(private val word: String) {
 
     init {
         require(word.length == 5) { "단어의 길이는 5글자여야 합니다." }
         require(Regex("[a-zA-Z]*").matches(word)) { "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다." }
+        // 게임이 시작되면 한번에 word.text를 가져와서 읽어온 다음에 비교하는게 낫다
+        // 매번 읽는거 -> 비효율적
+        // Words List<Word> <- words.text
     }
 }
--- src/test/kotlin/wordle/WordTest.kt
+++ src/test/kotlin/wordle/WordTest.kt
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class GameWordTest {
+class WordTest {
 
     @Test
     fun `5글자가 아닌 word를 생성하면 예외가 발생한다`() {
         val exception = shouldThrow<IllegalArgumentException> {
-            GameWord("test")
+            Word("test")
         }
         exception.message shouldBe "단어의 길이는 5글자여야 합니다."
     }
@@ -20,7 +20,7 @@ class GameWordTest {
     @ValueSource(strings = ["ap pl", "ap피le"])
     fun `영어가 아닌 글자가 포함된 word를 생성하면 예외가 발생한다`(word: String) {
         val exception = shouldThrow<IllegalArgumentException> {
-            GameWord(word)
+            Word(word)
         }
         exception.message shouldBe "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다."
     }
