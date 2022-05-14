c44d184690d6ddc661a84f230268c2a5d2dd8939
BeomsooKim <kbsat@naver.com>
2022-05-14 21:37:20 +0900

    refactor: println 두개로 나누어 표현

--- src/main/kotlin/wordle/view/OutputView.kt
+++ src/main/kotlin/wordle/view/OutputView.kt
@@ -5,10 +5,8 @@ import wordle.domain.Tile
 object OutputView {
 
     fun printStartMessage() {
-        println(
-            "WORDLE을 6번 만에 맞춰 보세요.\n" +
-            "시도의 결과는 타일의 색 변화로 나타납니다."
-        )
+        println("WORDLE을 6번 만에 맞춰 보세요.")
+        println("시도의 결과는 타일의 색 변화로 나타납니다.")
     }
 
     fun printResults(results: List<List<Tile>>) {
