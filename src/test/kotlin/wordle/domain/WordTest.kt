0a62335d1e87a8ba3c20aca3aa4a8d9992533a1f
summerlunaa <summerlunaa9@gmail.com>
2022-05-16 01:35:03 +0900

    test: shouldHaveMessage 메서드 적용

--- src/test/kotlin/wordle/domain/WordTest.kt
+++ src/test/kotlin/wordle/domain/WordTest.kt
@@ -3,25 +3,22 @@ package wordle.domain
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import org.junit.jupiter.api.Test
 
 class WordTest {
 
     @Test
     fun `5글자가 아닌 word를 생성하면 예외가 발생한다`() {
-        val exception = shouldThrow<IllegalArgumentException> {
-            Word("test")
-        }
-        exception.message shouldBe "단어의 길이는 5글자여야 합니다."
+        shouldThrow<IllegalArgumentException> { Word("test") }
+            .shouldHaveMessage("단어의 길이는 5글자여야 합니다.")
     }
 
     @Test
     fun `영어가 아닌 글자가 포함된 word를 생성하면 예외가 발생한다`() {
         listOf("ap pl", "ap피le").forAll {
-            val exception = shouldThrow<IllegalArgumentException> {
-                Word(it)
-            }
-            exception.message shouldBe "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다."
+            shouldThrow<IllegalArgumentException> { Word(it) }
+                .shouldHaveMessage("단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다.")
         }
     }
 
