6ab2de5c38a436eb4225a13507aa53599bb79c19
kth990303 <kth990303@naver.com>
2022-05-18 18:24:17 +0900

    refactor: `ktlintCheck` 컨벤션 변경

--- src/main/kotlin/study/dsl/Person.kt
+++ src/main/kotlin/study/dsl/Person.kt
@@ -3,7 +3,4 @@ package study.dsl
 import study.dsl.language.Languages
 import study.dsl.skill.Skills
 
-data class Person(
-    val name: String, val company: String,
-    val skills: Skills, val languages: Languages
-)
+data class Person(val name: String, val company: String, val skills: Skills, val languages: Languages)
