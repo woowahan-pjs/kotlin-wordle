80b79212d41ed11f10dc8f75338fb1828f4f819e
cjlee38 <cjlee38@github.com>
2022-05-19 13:51:37 +0900

    refactor: 검증 로직 확장함수로 변경

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -7,12 +7,12 @@ private const val WORD_LENGTH = 5
 data class Word(val word: String) {
     init {
         require(word.length == WORD_LENGTH) { "단어는 5글자여야 합니다." }
-        require(isLowerCase(word)) { "단어는 소문자로 이루어져야 합니다." }
-        require(contains(word)) { "유효하지 않은 단어입니다." }
+        require(word.isLowerCase()) { "단어는 소문자로 이루어져야 합니다." }
+        require(word.containsInWord()) { "유효하지 않은 단어입니다." }
     }
 
-    private fun isLowerCase(value: String): Boolean {
-        return value.all { it.isLowerCase() }
+    private fun String.isLowerCase(): Boolean {
+        return this.all { it.isLowerCase() }
     }
 
     fun compareByIndex(other: Word, myIndex: Int, otherIndex: Int = myIndex): Boolean {
@@ -23,8 +23,8 @@ data class Word(val word: String) {
         private val CACHE: List<String> = File("src/main/resources/words.txt")
             .readLines()
 
-        fun contains(word: String): Boolean {
-            return CACHE.contains(word)
+        fun String.containsInWord(): Boolean {
+            return CACHE.contains(this)
         }
 
         fun findWordByDay(day: Int): Word {
