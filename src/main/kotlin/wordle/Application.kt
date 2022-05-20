f45ae331204e481aa2ee97734d56e640ccca7607
summerlunaa <summerlunaa9@gmail.com>
2022-05-20 11:20:27 +0900

    refactor: 메서드명 수정

--- src/main/kotlin/wordle/Application.kt
+++ src/main/kotlin/wordle/Application.kt
@@ -9,17 +9,17 @@ import wordle.view.OutputView
 fun main() {
     val game = Game(WordsReader.getWords())
     OutputView.printStartMessage()
-    doGame(game)
+    playGame(game)
     OutputView.printCount(game.count)
     OutputView.printResults(game.results)
 }
 
-private fun doGame(game: Game) {
+private fun playGame(game: Game) {
     val answer = Word(InputView.inputAnswer())
     game.match(answer)
     if (game.isGameOver(answer)) {
         return
     }
     OutputView.printResults(game.results)
-    doGame(game)
+    playGame(game)
 }
