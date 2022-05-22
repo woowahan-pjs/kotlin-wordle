d7e35f8ca862204b7677797a0d91501dea19332a
summerlunaa <summerlunaa9@gmail.com>
2022-05-22 17:27:50 +0900

    refactor: pattern 상수화

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -6,10 +6,14 @@ data class Word(val value: String) {
 
     init {
         require(value.length == 5) { "단어의 길이는 5글자여야 합니다." }
-        require(Pattern.matches("[a-zA-Z]*", value)) { "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다." }
+        require(ENGLISH_PATTERN.matcher(value).matches()) { "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다." }
     }
 
     fun isSameChar(other: Word, index: Int): Boolean {
         return this.value[index] == other.value[index]
+    }
+
+    companion object {
+        val ENGLISH_PATTERN: Pattern = Pattern.compile("[a-zA-Z]*")
     }
 }
