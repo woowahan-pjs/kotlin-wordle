b58d19900b1b7674ad6c7aef0fd494e582aed533
dongho <dongho108@naver.com>
2022-05-26 14:37:41 +0900

    refactor: word 검증메서드들 확장함수로 변경

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -3,13 +3,13 @@ package wordle.domain
 data class Word(val value: String) {
 
     init {
-        require(isRightSize()) { "[ERROR] ${SIZE}글자의 단어를 입력하세요." }
-        require(isAlphabet()) { "[ERROR] 영어 단어를 입력하세요." }
+        require(value.isRightSize()) { "[ERROR] ${SIZE}글자의 단어를 입력하세요." }
+        require(value.isAlphabet()) { "[ERROR] 영어 단어를 입력하세요." }
     }
 
-    private fun isRightSize(): Boolean = value.length == SIZE
+    private fun String.isRightSize(): Boolean = value.length == SIZE
 
-    private fun isAlphabet(): Boolean = Regex("^[a-zA-Z]*$").matches(value)
+    private fun String.isAlphabet(): Boolean = Regex("^[a-zA-Z]*$").matches(value)
 
     fun sameIndexAndSpell(index: Int, spell: Char): Boolean = value[index] == spell
 
