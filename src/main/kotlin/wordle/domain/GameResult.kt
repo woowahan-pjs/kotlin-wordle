07e75fa337596408d0c164416eb6eb91fe24e04f
dongho <dongho108@naver.com>
2022-05-26 14:19:58 +0900

    refactor: ArrayList의 InitialCapacity를 미리 지정하도록 수정

--- src/main/kotlin/wordle/domain/GameResult.kt
+++ src/main/kotlin/wordle/domain/GameResult.kt
@@ -2,10 +2,14 @@ package wordle.domain
 
 class GameResult() {
 
-    var gameResult: MutableList<Tiles> = ArrayList()
+    var gameResult: MutableList<Tiles> = ArrayList(SIZE)
         private set
 
     fun add(tiles: Tiles) {
         this.gameResult.add(tiles)
+    }
+
+    companion object {
+        private const val SIZE = 5
     }
 }
