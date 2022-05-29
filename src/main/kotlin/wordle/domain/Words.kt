3e3219e154a668afe87e295a54f9ef7799f62bf7
SuyeonChoi <tndus970120@gmail.com>
2022-05-30 02:19:48 +0900

    refactor(Words): object 클래스로 변경

--- src/main/kotlin/wordle/domain/Words.kt
+++ src/main/kotlin/wordle/domain/Words.kt
@@ -6,19 +6,18 @@ import java.time.temporal.ChronoUnit
 
 const val WORD_SIZE = 5
 
-class Words {
+object Words {
 
-    companion object {
-        private val VALUE: List<String> = File("src/main/resources/words.txt").readLines()
-        private val BASIC_DATE = LocalDate.of(2021, 6, 19)
+    private val VALUE: List<String> = File("src/main/resources/words.txt").readLines()
 
-        fun contains(word: String): Boolean {
-            return VALUE.contains(word)
-        }
+    private val BASIC_DATE = LocalDate.of(2021, 6, 19)
 
-        fun pick(date: LocalDate): String {
-            val index = ChronoUnit.DAYS.between(BASIC_DATE, date) % VALUE.size
-            return VALUE[index.toInt()]
-        }
+    fun contains(word: String): Boolean {
+        return VALUE.contains(word)
+    }
+
+    fun pick(date: LocalDate): String {
+        val index = ChronoUnit.DAYS.between(BASIC_DATE, date) % VALUE.size
+        return VALUE[index.toInt()]
     }
 }
