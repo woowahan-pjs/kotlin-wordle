50c25d1f7a1bf85d00473655b965079092f4b26b
BeomsooKim <kbsat@naver.com>
2022-05-29 22:42:36 +0900

    refactor: Word 확장함수 적용

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -7,12 +7,20 @@ class Word(_value: String) {
     val value: String = _value.lowercase()
 
     init {
-        require(value.length == WORD_SIZE) { "단어의 길이는 5글자여야 합니다." }
-        require(Regex("[a-zA-Z]*").matches(value)) { "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다." }
+        require(value.isRightSize()) { "단어의 길이는 5글자여야 합니다." }
+        require(value.isAlphabet()) { "단어에 영어가 아닌 글자나 공백이 포함될 수 없습니다." }
     }
 
     fun isSameChar(other: Word, index: Int): Boolean {
         return this.value[index] == other.value[index]
+    }
+
+    private fun String.isRightSize(): Boolean {
+        return this.length == WORD_SIZE
+    }
+
+    private fun String.isAlphabet(): Boolean {
+        return Regex("[a-zA-Z]*").matches(this)
     }
 
     override fun equals(other: Any?): Boolean {
