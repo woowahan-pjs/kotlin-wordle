6e784729aebf3ddca1135598603680bb92153c4a
summerlunaa <summerlunaa9@gmail.com>
2022-06-03 01:59:12 +0900

    refactor: 답안에 대한 타일 결과를 구하는 로직 변경

--- src/main/kotlin/wordle/domain/Words.kt
+++ src/main/kotlin/wordle/domain/Words.kt
@@ -5,7 +5,6 @@ import java.time.LocalDate
 class Words(private val values: List<Word>, today: LocalDate = LocalDate.now()) {
 
     private val answer: Word = findAnswer(today)
-    private var answerMap: Map<Char, Int> = mapOf()
 
     private fun findAnswer(date: LocalDate): Word {
         val days = date.compareTo(STANDARD_DATE)
@@ -19,53 +18,39 @@ class Words(private val values: List<Word>, today: LocalDate = LocalDate.now()) 
     }
 
     fun check(word: Word): List<Tile> {
-        answerMap = initAnswerMap()
-        return DEFAULT_RESULT.markGreen(word).markYellow(word)
+        return greenOrGray(word).yellowOrExist(word)
     }
 
-    private fun initAnswerMap(): Map<Char, Int> {
-        return answer.value
-            .groupBy { it }
-            .mapValues { it.value.count() }
-    }
-
-    private fun List<Tile>.markGreen(word: Word): List<Tile> {
-        return mapIndexed { index, tile -> grayOrGreen(tile, word, index) }
-    }
-
-    private fun grayOrGreen(tile: Tile, word: Word, index: Int): Tile {
-        return if (answer.isSameChar(word, index)) {
-            updateAnswerMap(word.value[index])
-            Tile.GREEN
-        } else {
-            tile
+    private fun greenOrGray(word: Word): List<Tile> {
+        return word.value.mapIndexed { index, letter ->
+            if (answer.value[index] == letter) {
+                Tile.GREEN
+            } else {
+                Tile.GRAY
+            }
         }
     }
 
-    private fun List<Tile>.markYellow(word: Word): List<Tile> {
-        return mapIndexed { index, tile -> existOrYellow(tile, word, index) }
-    }
+    private fun List<Tile>.yellowOrExist(word: Word): List<Tile> {
+        var notCheckedLetters = answer.value.filterIndexed { index, _ -> get(index) != Tile.GREEN }
 
-    private fun existOrYellow(tile: Tile, word: Word, index: Int): Tile {
-        return if (tile != Tile.GREEN && answerMap.containsKey(word.value[index])) {
-            updateAnswerMap(word.value[index])
-            Tile.YELLOW
-        } else {
-            tile
+        return mapIndexed { index, tile ->
+            val letter = word.value[index]
+            if (letter in notCheckedLetters && tile != Tile.GREEN) {
+                notCheckedLetters = notCheckedLetters.removeFirst(letter)
+                Tile.YELLOW
+            } else {
+                tile
+            }
         }
     }
 
-    private fun updateAnswerMap(letter: Char) {
-        answerMap = answerMap
-            .mapValues { it.reduceIfSame(letter) }
-            .filter { it.value != 0 }
+    private fun String.removeFirst(letter: Char): String {
+        return replaceFirst(letter.toString(), "")
     }
-
-    private fun Map.Entry<Char, Int>.reduceIfSame(letter: Char) = if (key == letter) value - 1 else value
 
     companion object {
-        const val WORD_SIZE = 5
-        private val DEFAULT_RESULT = List(WORD_SIZE) { Tile.GRAY }
-        private val STANDARD_DATE = LocalDate.of(2021, 6, 19)
+        const val WORD_SIZE: Int = 5
+        private val STANDARD_DATE: LocalDate = LocalDate.of(2021, 6, 19)
     }
 }
