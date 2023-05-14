01c6b399798d354dca6c18876b08d211332f8675
Choi-JJunho <junho5336@gmail.com>
2023-05-14 15:14:55 +0900

    fix: match 오류수정
    Co-authored-by: cfalws <akdlzmf1123@gmail.com>
    Co-authored-by: Choisehyeon <chltpgus6275@gmail.com>

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -2,14 +2,14 @@ package wordle.domain
 
 data class Word private constructor(val word: List<Letter>) {
     init {
-        require(word.size == 5) { "단어는 5글자여야 합니다." }
+        require(word.size == WORD_SIZE) { "단어는 " + WORD_SIZE + "글자여야 합니다." }
     }
 
     fun match(other: Word): Result {
-        var result: MutableList<MatchResult> = MutableList(5) { MatchResult.GRAY }
+        val result: MutableList<MatchResult> = MutableList(5) { MatchResult.GRAY }
 
         val greenPositions: List<Int> = checkGreen(other.word)
-        val yellowPositions: List<Int> = checkYellow(other.word, greenPositions)
+        val yellowPositions: List<Int> = checkYellow(other.word, greenPositions.toMutableList())
 
         greenPositions.forEach { result[it] = MatchResult.GREEN }
         yellowPositions.forEach { result[it] = MatchResult.YELLOW }
@@ -17,22 +17,32 @@ data class Word private constructor(val word: List<Letter>) {
     }
 
     private fun checkGreen(other: List<Letter>): List<Int> =
-        other.filter { this.match(it, listOf()) == MatchResult.GREEN }.map { it.position }
+        other.filter { this.match(it, mutableListOf()) == MatchResult.GREEN }.map { it.position }
 
-    private fun checkYellow(other: List<Letter>, excludeIndex: List<Int>): List<Int> =
-        other.filter { excludeIndex.contains(it.position).not() && this.match(it, excludeIndex) == MatchResult.YELLOW }
-            .map { it.position }
-
-    private fun match(other: Letter, excludeIndex: List<Int>): MatchResult {
-        val matchResults = word.filterNot { excludeIndex.contains(it.position) }.map { it.match(other) }
-        return when {
-            matchResults.contains(MatchResult.GREEN) -> MatchResult.GREEN
-            matchResults.contains(MatchResult.YELLOW) -> MatchResult.YELLOW
-            else -> MatchResult.GRAY
+    private fun checkYellow(other: List<Letter>, excludeGreenIndex: MutableList<Int>): List<Int> {
+        val excludeYellowIndex: MutableList<Int> = excludeGreenIndex.toMutableList()
+        return other.filter {
+            excludeGreenIndex.contains(it.position).not() && this.match(
+                it,
+                excludeYellowIndex
+            ) == MatchResult.YELLOW
         }
+            .map { it.position }
+    }
+
+    private fun match(other: Letter, excludeIndex: MutableList<Int>): MatchResult {
+        word.filterNot { excludeIndex.contains(it.position) }
+            .filterNot { it.match(other) == MatchResult.GRAY }
+            .forEach {
+                excludeIndex.add(it.position)
+                return it.match(other)
+            }
+        return MatchResult.GRAY
     }
 
     companion object {
+        private const val WORD_SIZE = 5
+
         fun from(word: String): Word {
             require(WordPool.words.contains(word)) { "허용되지 않는 단어입니다." }
             return Word(word.mapIndexed { index, letter -> Letter(index, letter) })
