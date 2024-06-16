73296da005ff78c2fda638bf37a67c592ae8c1b5
woo-yu <hjwoou@gmail.com>
2024-06-16 22:45:31 +0900

    fix: Step 패키지 변경 및 answer, word 포함

---
+++ src/main/kotlin/wordle/Step.kt
@@ -0,0 +1,48 @@
+package wordle
+
+/**
+ * 사용자가 [Word]를 입력하여 정답과 비교한 결과를 가진 한 번의 단계
+ */
+data class Step(val answer: String, val word: Word) {
+
+    enum class Result {
+        CORRECT,
+        MISMATCH,
+        WRONG;
+    }
+
+    val result: List<Result>
+
+    init {
+        result = if (answer == word.value) {
+             List(5) { Result.CORRECT }.toList()
+        } else {
+            MutableList(5) { Result.WRONG }.apply {
+                fillCorrect(this)
+                fillMismatch(this)
+            }
+        }
+    }
+
+    val isCorrect = result.all { it == Result.CORRECT }
+
+    private fun fillCorrect(initResult: MutableList<Result>) {
+        word.value.forEachIndexed { index, c ->
+            if (answer[index] == c) {
+                initResult[index] = Result.CORRECT
+            }
+        }
+    }
+
+    private fun fillMismatch(correctedResult: MutableList<Result>) {
+        val calculatedAnswer = StringBuilder(answer.filterIndexed { index, _ ->  correctedResult[index] != Result.CORRECT })
+
+        word.value.forEachIndexed { index, c ->
+            if (correctedResult[index] != Result.CORRECT && calculatedAnswer.contains(c)) {
+                correctedResult[index] = Result.MISMATCH
+                val foundIndex = calculatedAnswer.indexOf(c)
+                calculatedAnswer.deleteAt(foundIndex)
+            }
+        }
+    }
+}
