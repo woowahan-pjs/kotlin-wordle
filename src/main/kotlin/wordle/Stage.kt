4c43baac5e3365b1816cafb949364f3910874bdf
woo-yu <hjwoou@gmail.com>
2024-06-16 23:22:43 +0900

    fix: Stage 패키지 변경 및 state 연산을 초기화시 진행

---
+++ src/main/kotlin/wordle/Stage.kt
@@ -0,0 +1,32 @@
+package wordle
+
+/**
+ * 사용자가 하나의 정답을 가지고 진행하는 한
+ */
+data class Stage(val answer: String, val steps: List<Step> = listOf()) {
+
+    enum class State {
+        PROGRESS, COMPLETE, FAIL
+    }
+
+    val state: State = when {
+        steps.any { step -> step.isCorrect } -> State.COMPLETE
+        steps.size == 6 -> State.FAIL
+        else -> State.PROGRESS
+    }
+
+    val finished = state != State.PROGRESS
+
+    fun play(word: Word): Stage {
+        if (finished) return this
+        val step = Step(answer, word)
+        val newSteps = steps.toMutableList().apply {
+            add(step)
+        }
+
+        return Stage(
+            answer = answer,
+            steps = newSteps,
+        )
+    }
+}
---
+++ src/test/kotlin/wordle/StageTest.kt
@@ -0,0 +1,62 @@
+package wordle
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import wordle.Stage
+import wordle.Word
+
+internal class StageTest {
+
+    @Test
+    fun `스테이지를 만들면 PROGRESS`() {
+        val stage = Stage("hello")
+
+        assertThat(stage.state).isEqualTo(Stage.State.PROGRESS)
+        assertThat(stage.finished).isFalse()
+    }
+
+    @Test
+    fun `스테이지가 play시 정답을 맞추면 Complete`() {
+        // given
+        val stage = Stage("hello")
+
+        // when
+        val newStage = stage.play(Word("hello"))
+
+        // then
+        assertThat(newStage.state).isEqualTo(Stage.State.COMPLETE)
+        assertThat(newStage.finished).isTrue()
+    }
+
+    @Test
+    fun `스테이지가 play시 정답을 맞추지 못하면 Complete`() {
+        // given
+        val stage = Stage("hello")
+
+        // when
+        val newStage = stage.play(Word("wrong"))
+
+        // then
+        assertThat(newStage.state).isEqualTo(Stage.State.PROGRESS)
+        assertThat(newStage.finished).isFalse()
+    }
+
+    @Test
+    fun `스테이지가 play시 6번 모두 정답을 맞추지 못하면 FAIL`() {
+        // given
+        val stage = Stage("hello")
+
+        //when
+        val failedStage = stage
+            .play(Word("wrong"))
+            .play(Word("wrong"))
+            .play(Word("wrong"))
+            .play(Word("wrong"))
+            .play(Word("wrong"))
+            .play(Word("wrong"))
+
+        // then
+        assertThat(failedStage.state).isEqualTo(Stage.State.FAIL)
+        assertThat(failedStage.finished).isTrue()
+    }
+}
