3e757649afca668b5192c634e8e822b3c672acd6
boradol2 <boradol0902@gmail.com>
2024-06-17 16:37:18 +0900

    feat(Word): 단어는 여러 개의 글자로 이루어진다.

--- src/main/kotlin/wordle/domain/Word.kt
+++ src/main/kotlin/wordle/domain/Word.kt
@@ -4,14 +4,16 @@ import wordle.exception.WordleExceptionCode.WORD_INVALID_LENGTH
 import wordle.exception.WordleExceptionCode.WORD_IS_NOT_IN_DICTIONARY
 import wordle.exception.WordleExceptionCode.WORD_NOT_ALLOW_SPACE
 
-data class Word(val word: String) {
-    init {
-        check(word.isNotBlank()) { WORD_NOT_ALLOW_SPACE.message }
-        check(isValidLength()) { WORD_INVALID_LENGTH.message }
-        check(isDictionaryWord(word)) { WORD_IS_NOT_IN_DICTIONARY.message }
-    }
-
-    private fun isValidLength(): Boolean = word.length == WORD_LENGTH
-}
+data class Word(private val word: List<Letter>)
 
 const val WORD_LENGTH = 5
+
+fun Word(word: String): Word {
+    check(word.isNotBlank()) { WORD_NOT_ALLOW_SPACE.message }
+    check(isValidLength(word)) { WORD_INVALID_LENGTH.message }
+    check(isDictionaryWord(word)) { WORD_IS_NOT_IN_DICTIONARY.message }
+
+    return Word(word.toCharArray().map { Letter(it) })
+}
+
+private fun isValidLength(word: String) = word.length == WORD_LENGTH
