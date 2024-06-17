9299ecae2bda4fab4624a770e6c2c9735e7454ee
boradol2 <boradol0902@gmail.com>
2024-06-18 07:04:58 +0900

    feat(WordResult): 단어 결과는 단어 길이와 일치해야 합니다.

--- src/main/kotlin/wordle/domain/WordResult.kt
+++ src/main/kotlin/wordle/domain/WordResult.kt
@@ -1,6 +1,12 @@
 package wordle.domain
 
+import wordle.exception.WordleExceptionCode.WORD_RESULT_INVALID_LENGTH
+
 data class WordResult(private val result: MutableList<LetterMatch> = MutableList(WORD_LENGTH) { LetterMatch.ABSENT }) {
+    init {
+        check(result.size == WORD_LENGTH) { WORD_RESULT_INVALID_LENGTH.message }
+    }
+
     fun changeMatchType(
         index: Int,
         matchType: LetterMatch,
--- src/main/kotlin/wordle/exception/WordleExceptionCode.kt
+++ src/main/kotlin/wordle/exception/WordleExceptionCode.kt
@@ -8,4 +8,5 @@ enum class WordleExceptionCode(val message: String) {
     WORD_IS_NOT_IN_DICTIONARY("Wordle Game에서 유효한 단어가 아닙니다."),
     LETTER_INVALID_CHARACTER_TYPE("유효하지 않은 글자 형식입니다."),
     TRY_COUNT_HAS_NOT_REMAINDER("시행 횟수는 0보다 작을 수 없습니다."),
+    WORD_RESULT_INVALID_LENGTH("단어 결과의 글자 일치 상태 목록들은 단어 길이인 ${WORD_LENGTH}와 일치해야 합니다."),
 }
